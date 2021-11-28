66c285548b0a4b41a64cbc66e33e2a97ace79656
Daniel <danbapp@gmail.com>
2021-11-28 16:16:53 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  // find the length of a string
+  const len = word.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+
+      // check if first and last string are same
+      if (word[i] !== word[len - 1 - i]) {
+          return false;
+      }
+  }
+  return true;
 }
+
+// isPalindrome('list');
 
 /* 
   Add your pseudocode here
+
+  function
+  check string length
+  check beginning through middle character
+  check end through middle character
+  If beginning and end are equal true, if not false
 */
 
 /*
   Add written explanation of your solution here
+
+  Create a function that determines whehter a word is a palindrome, returning a true or false
 */
 
 // You can run `node index.js` to view these console logs
